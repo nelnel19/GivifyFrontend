e26d1ba531b760d6ec6556aa87732284fc0ceb06
Arnel <arnelvergara1234@gmail.com>
2025-10-04 20:41:35 +0800

    register2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/auth", // Flask backend
+  baseURL: "https://givifybackend.onrender.com/api/auth", // Flask backend
 });
 
 export default API;
